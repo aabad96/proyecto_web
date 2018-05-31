f01a7a7046979e5c019a0066c739a0b985f36d5a
Alejandro Abad <aabad96@icloud.com>
2018-05-31 12:54:49 +0200

    V2.3 Añadido contribuir a BBDD
    Ya se completa  la contribucion con extension

---
+++ php/contribuir.php
@@ -0,0 +1,54 @@
+<?php
+	define("DB_SERVER", "18.191.39.0");
+	define("DB_USER", "publico");
+	define("DB_PASS", "1234");
+	define("DB_NAME", "proyectoWeb");
+
+  // 1. Crear conexión con la BBDD
+  $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+  // Test if connection succeeded
+  if(mysqli_connect_errno()) {
+    die("La conexión con la BBDD ha fallado: " . 
+         mysqli_connect_error() . 
+         " (" . mysqli_connect_errno() . ")"
+    );
+  }
+?>
+
+<?php
+
+if(isset($_POST['email'])) { 
+    // check if the username has been set
+	$email = $_POST["email"];
+}
+if(isset($_POST['tipo'])) { 
+    // check if the username has been set
+	$tipo = $_POST["tipo"];
+}
+if(isset($_POST['comentario'])) { 
+    // check if the username has been set
+	$comentario = $_POST["comentario"];
+}
+
+$tablename ="sugerencia";
+        //Encriptar password
+        $query  = "INSERT INTO `$tablename` (";
+		$query .= "  `email`, `tipo`,`comentario`";
+		$query .= ") VALUES (";
+		$query .= " '$email', '$tipo', '$comentario'";
+		$query .= ")";
+	
+		$result = mysqli_query($connection, $query);
+
+			if ($result) {
+				echo "Gracias por tu colaboración '$email'";
+			} else {
+				die("Database query failed. " . mysqli_error($connection));
+	}
+	
+?>
+
+<?php
+  // 5. Close database connection
+  mysqli_close($connection);
+?>
